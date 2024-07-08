72d5fd565620a31e9f8a17878738e00b909d1ed8
mou-wj <100018914+mou-wj@users.noreply.github.com>
2024-07-08 21:52:03 +0800

    To
    memory allocation p908

--- src/test/MemoryAllocationTest.cpp
+++ src/test/MemoryAllocationTest.cpp
@@ -113,6 +113,80 @@ void MemoryAllocationTest::HostAllocationCallbacksTest()
 
 }
 
+
+
+
+struct MemoryAllocateInfoEXT {
+	VkDedicatedAllocationMemoryAllocateInfoNV dedicatedAllocationMemoryAllocateInfoNV{};
+	VkExportMemoryAllocateInfo exportMemoryAllocateInfo{};
+	VkExportMemoryAllocateInfoNV exportMemoryAllocateInfoNV{};
+	VkExportMemoryWin32HandleInfoKHR exportMemoryWin32HandleInfoKHR{};
+	VkExportMemoryWin32HandleInfoNV exportMemoryWin32HandleInfoNV{};
+	VkExportMetalObjectCreateInfoEXT exportMetalObjectCreateInfoEXT{};
+	VkImportAndroidHardwareBufferInfoANDROID importAndroidHardwareBufferInfoANDROID{};
+	VkImportMemoryBufferCollectionFUCHSIA importMemoryBufferCollectionFUCHSIA{};
+	VkImportMemoryFdInfoKHR importMemoryFdInfoKHR{};
+	VkImportMemoryHostPointerInfoEXT importMemoryHostPointerInfoEXT{};
+	VkImportMemoryWin32HandleInfoKHR importMemoryWin32HandleInfoKHR{};
+	VkImportMemoryWin32HandleInfoNV importMemoryWin32HandleInfoNV{};
+	VkImportMemoryZirconHandleInfoFUCHSIA importMemoryZirconHandleInfoFUCHSIA{};
+	VkImportMetalBufferInfoEXT importMetalBufferInfoEXT{};
+	VkImportScreenBufferInfoQNX importScreenBufferInfoQNX{};
+	VkMemoryAllocateFlagsInfo memoryAllocateFlagsInfo{};
+	VkMemoryDedicatedAllocateInfo memoryDedicatedAllocateInfo{};
+	VkMemoryOpaqueCaptureAddressAllocateInfo memoryOpaqueCaptureAddressAllocateInfo{};
+	VkMemoryPriorityAllocateInfoEXT memoryPriorityAllocateInfoEXT{};
+	MemoryAllocateInfoEXT() {
+		Init();
+	}
+	void Init() {
+		//这里只是示例，有一些结构体是不能够同时链接到一起链接到VkMemoryAllocateInfo的pNext中的
+		dedicatedAllocationMemoryAllocateInfoNV.sType = VK_STRUCTURE_TYPE_DEDICATED_ALLOCATION_MEMORY_ALLOCATE_INFO_NV;
+		dedicatedAllocationMemoryAllocateInfoNV.pNext = &exportMemoryAllocateInfo;
+		exportMemoryAllocateInfo.sType = VK_STRUCTURE_TYPE_EXPORT_MEMORY_ALLOCATE_INFO;
+		exportMemoryAllocateInfo.pNext = &exportMemoryAllocateInfoNV;
+		exportMemoryAllocateInfoNV.sType = VK_STRUCTURE_TYPE_EXPORT_MEMORY_ALLOCATE_INFO_NV;
+		exportMemoryAllocateInfoNV.pNext = &exportMemoryWin32HandleInfoKHR;
+		exportMemoryWin32HandleInfoKHR.sType = VK_STRUCTURE_TYPE_EXPORT_MEMORY_WIN32_HANDLE_INFO_KHR;
+		exportMemoryWin32HandleInfoKHR.pNext = &exportMemoryWin32HandleInfoNV;
+		exportMemoryWin32HandleInfoNV.sType = VK_STRUCTURE_TYPE_EXPORT_MEMORY_WIN32_HANDLE_INFO_NV;
+		exportMemoryWin32HandleInfoNV.pNext = &exportMetalObjectCreateInfoEXT;
+		exportMetalObjectCreateInfoEXT.sType = VK_STRUCTURE_TYPE_EXPORT_METAL_OBJECT_CREATE_INFO_EXT;
+		exportMetalObjectCreateInfoEXT.pNext = &importAndroidHardwareBufferInfoANDROID;
+		importAndroidHardwareBufferInfoANDROID.sType = VK_STRUCTURE_TYPE_IMPORT_ANDROID_HARDWARE_BUFFER_INFO_ANDROID;
+		importAndroidHardwareBufferInfoANDROID.pNext = &importMemoryBufferCollectionFUCHSIA;
+		importMemoryBufferCollectionFUCHSIA.sType = VK_STRUCTURE_TYPE_IMPORT_MEMORY_BUFFER_COLLECTION_FUCHSIA;
+		importMemoryBufferCollectionFUCHSIA.pNext = &importMemoryFdInfoKHR;
+		importMemoryFdInfoKHR.sType = VK_STRUCTURE_TYPE_IMPORT_MEMORY_FD_INFO_KHR;
+		importMemoryFdInfoKHR.pNext = &importMemoryHostPointerInfoEXT;
+		importMemoryHostPointerInfoEXT.sType = VK_STRUCTURE_TYPE_IMPORT_MEMORY_HOST_POINTER_INFO_EXT;
+		importMemoryHostPointerInfoEXT.pNext = &importMemoryWin32HandleInfoKHR;
+		importMemoryWin32HandleInfoKHR.sType = VK_STRUCTURE_TYPE_IMPORT_MEMORY_WIN32_HANDLE_INFO_KHR;
+		importMemoryWin32HandleInfoKHR.pNext = &importMemoryWin32HandleInfoNV;
+		importMemoryWin32HandleInfoNV.sType = VK_STRUCTURE_TYPE_IMPORT_MEMORY_WIN32_HANDLE_INFO_NV;
+		importMemoryWin32HandleInfoNV.pNext = &importMemoryZirconHandleInfoFUCHSIA;
+		importMemoryZirconHandleInfoFUCHSIA.sType = VK_STRUCTURE_TYPE_IMPORT_MEMORY_ZIRCON_HANDLE_INFO_FUCHSIA;
+		importMemoryZirconHandleInfoFUCHSIA.pNext = &importMetalBufferInfoEXT;
+		importMetalBufferInfoEXT.sType = VK_STRUCTURE_TYPE_IMPORT_METAL_BUFFER_INFO_EXT;
+		importMetalBufferInfoEXT.pNext = &importScreenBufferInfoQNX;
+		importScreenBufferInfoQNX.sType = VK_STRUCTURE_TYPE_IMPORT_SCREEN_BUFFER_INFO_QNX;
+		importScreenBufferInfoQNX.pNext = &memoryAllocateFlagsInfo;
+		memoryAllocateFlagsInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_FLAGS_INFO;
+		memoryAllocateFlagsInfo.pNext = &memoryDedicatedAllocateInfo;
+		memoryDedicatedAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO;
+		memoryDedicatedAllocateInfo.pNext = &memoryOpaqueCaptureAddressAllocateInfo;
+		memoryOpaqueCaptureAddressAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_OPAQUE_CAPTURE_ADDRESS_ALLOCATE_INFO;
+		memoryOpaqueCaptureAddressAllocateInfo.pNext = &memoryPriorityAllocateInfoEXT;
+		memoryPriorityAllocateInfoEXT.sType = VK_STRUCTURE_TYPE_MEMORY_PRIORITY_ALLOCATE_INFO_EXT;
+		memoryPriorityAllocateInfoEXT.pNext = nullptr;
+
+	}
+
+};
+
+
+
+
 void MemoryAllocationTest::DeviceMemoryTest()
 {
 
@@ -193,6 +267,71 @@ void MemoryAllocationTest::DeviceMemoryTest()
 	memoryAllocateInfo.pNext = nullptr;
 	memoryAllocateInfo.allocationSize = 1;//只需要分配的内存的字节数
 	memoryAllocateInfo.memoryTypeIndex = 0;//是从vkGetPhysicalDeviceMemoryProperties获取的VkPhysicalDeviceMemoryProperties结构的memoryTypes数组中的内存类型的索引。
+	/*
+	VkMemoryAllocateInfo有效用法：
+	1.如果参数没有指定一个import 或export operation，则allocationSize 必须大于0
+	2.参数不能指定多个import 操作
+	3.如果参数定义了一个export operation,且handleType不是VK_EXTERNAL_MEMORY_HANDLE_TYPE_ANDROID_HARDWARE_BUFFER_BIT_ANDROID，则allocationSize必须大于0
+	4.如果参数定义了一个导入VkBufferCollectionFUCHSIA的import operation,则（1）如果VkMemoryDedicatedAllocateInfo::buffer未非NULL的值，VkImportMemoryBufferCollectionFUCHSIA::collection 以及VkImportMemoryBufferCollectionFUCHSIA::index 必须各自匹配和创建VkMemoryDedicatedAllocateInfo::buffer的VkBufferCollectionBufferCreateInfoFUCHSIA结构体中的 VkBufferCollectionBufferCreateInfoFUCHSIA::collection 和VkBufferCollectionBufferCreateInfoFUCHSIA::index
+																		  （2）如果VkMemoryDedicatedAllocateInfo::image未非NULL的值，VkImportMemoryBufferCollectionFUCHSIA::collection 以及VkImportMemoryBufferCollectionFUCHSIA::index 必须各自匹配和创建VkMemoryDedicatedAllocateInfo::image的VkBufferCollectionBufferCreateInfoFUCHSIA结构体中的 VkBufferCollectionBufferCreateInfoFUCHSIA::collection 和VkBufferCollectionBufferCreateInfoFUCHSIA::index
+																		  （3）则allocationSize必须和vkGetImageMemoryRequirements或者vkGetBufferMemoryRequirements调用获取的image-based 或buffer-based collections的VkMemoryRequirements::size相匹配。
+																		  （4）则pNext中必须包含一个其image 或者 buffer 对象不为VK_NULL_HANDLE的VkMemoryDedicatedAllocateInfo结构体
+																		  （5）如果pNext中包含VkMemoryDedicatedAllocateInfo结构体，则VkMemoryDedicatedAllocateInfo::image创建时必须在VkImageCreateInfo::pNext中包含一个VkBufferCollectionImageCreateInfoFUCHSIA结构体，
+																		  （6）如果pNext中包含VkMemoryDedicatedAllocateInfo结构体，则VkMemoryDedicatedAllocateInfo::buffer创建时必须在VkBufferCreateInfo::pNext中包含一个VkBufferCollectionImageCreateInfoFUCHSIA结构体，
+																		  （7）则memoryTypeIndex 必须是从vkGetBufferCollectionPropertiesFUCHSIA 调用获取的VkBufferCollectionPropertiesFUCHSIA的对应内存类型索引。
+	5.如果pNext 中包含了一个VkExportMemoryAllocateInfo，则（1）如果其image或者 buffer不为VK_NULL_HANDLE ，且任何VkExportMemoryAllocateInfo::handleTypes 指定的handle type 需要独占分配，是否独占分配对image可以通过vkGetPhysicalDeviceImageFormatProperties2调用返回的VkExternalImageFormatProperties::externalMemoryProperties.externalMemoryFeatures 或者对于buffer，通过调用vkGetPhysicalDeviceExternalBufferProperties获取的 VkExternalBufferProperties::externalMemoryProperties.externalMemoryFeatures 中进行呈现，则pNext中必须包含一个VkMemoryDedicatedAllocateInfo 或者VkDedicatedAllocationMemoryAllocateInfoNV
+														  （2）pNext中就不能再包含一个 VkExportMemoryAllocateInfoNV 或者 VkExportMemoryWin32HandleInfoNV
+	6.如果pNext 中包含了一个VkImportMemoryWin32HandleInfoKHR， 则pNext就不能包含 VkImportMemoryWin32HandleInfoNV
+
+	7.如果参数定义了一个import operation,且external handle 是通过Vulkan API创建的，则（1）且这个external handle的类型为VK_EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_FD_BIT，则allocationSize 和memoryTypeIndex 就必须和被引入的payload创建时指定的参数相匹配。
+																					 （2）VkMemoryAllocateFlagsInfo指定的 device mask必须和被引入的payload创建时指定的参数相匹配。
+																					 （3）则传给vkAllocateMemory的 逻辑设备的 physical devices列表必须和被引入的payload分配时传入的physical devices列表相匹配。
+																					 （4）如果external handle的类型为VK_EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_WIN32_BIT 或者VK_EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_WIN32_KMT_BIT， 则allocationSize 和memoryTypeIndex 就必须和被引入的payload创建时指定的参数相匹配。
+	8.如果参数定义了一个import operation，且external handle 为Vulkan API外部创建 NT handle 或者一个的global share handle，则memoryTypeIndex 必须是vkGetMemoryWin32HandlePropertiesKHR 返回信息中对应参数中的一个的索引																
+	9.如果参数定义了一个import operation，且external handle 类型为VK_EXTERNAL_MEMORY_HANDLE_TYPE_D3D12_HEAP_BIT，则allocationSize 必须和从被提取出的payload中创建Direct3D 12 heap payload时指定的参数相匹配。
+	10.如果参数定义了一个import operation，且external handle 是一个Vulkan API外创建的POSIX file descriptor，则memoryTypeIndex 必须是vkGetMemoryFdPropertiesKHR 返回信息中对应参数中的一个的索引
+	11.如果protectedMemory 特性没有开启，则VkMemoryAllocateInfo::memoryTypeIndex 不能指向一个含VK_MEMORY_PROPERTY_PROTECTED_BIT 类型的内存类型
+	12.如果参数定义了一个import operation，且external handle 为一个host pointer， 则（1）memoryTypeIndex 必须是vkGetMemoryHostPointerPropertiesEXT 返回信息中对应参数中的一个的索引
+																					（2）allocationSize 必须是一个整数倍的VkPhysicalDeviceExternalMemoryHostPropertiesEXT::minImportedHostPointerAlignment
+																					（3）pNext中不能包含一个其image 或者 buffer 对象不为VK_NULL_HANDLE的VkDedicatedAllocationMemoryAllocateInfoNV结构体
+																					（4）pNext中不能包含一个其image 或者 buffer 对象不为VK_NULL_HANDLE的VkMemoryDedicatedAllocateInfo结构体
+	13.如果参数定义了一个 import operation，且external handle 类型为VK_EXTERNAL_MEMORY_HANDLE_TYPE_ANDROID_HARDWARE_BUFFER_BIT_ANDROID	，则（1）对于Android hardware buffer，allocationSize必须是从vkGetAndroidHardwareBufferPropertiesANDROID 中返回的值																	
+																																			（2）如果pNext中不包含一个VkMemoryDedicatedAllocateInfo 或者VkMemoryDedicatedAllocateInfo::image为VK_NULL_HANDLE，Android hardware buffer 就必须有AHardwareBuffer_Desc::format为AHARDWAREBUFFER_FORMAT_BLOB 以及AHardwareBuffer_Desc::usage 包含AHARDWAREBUFFER_USAGE_GPU_DATA_BUFFER
+																																			（3）对于Android hardware buffer， memoryTypeIndex 必须是从vkGetAndroidHardwareBufferPropertiesANDROID 返回的参数中所对应参数的的索引
+	14.如果参数没有指定一个import operation，且pNext 中包含一个handleTypes包含VK_EXTERNAL_MEMORY_HANDLE_TYPE_ANDROID_HARDWARE_BUFFER_BIT_ANDROID的 VkExportMemoryAllocateInfo，且pNext中包含一个image 不为VK_NULL_HANDLE的VkMemoryDedicatedAllocateInfo，则allocationSize 必须是0
+	15.如果参数定义了一个export operation，且handle类型为VK_EXTERNAL_MEMORY_HANDLE_TYPE_ANDROID_HARDWARE_BUFFER_BIT_ANDROID，则（1）如果pNext中不含VkMemoryDedicatedAllocateInfo，则allocationSize 必须大于 0
+																															   （2）如果pNext中包含一个buffer为有效值的VkMemoryDedicatedAllocateInfo，则allocationSize 必须大于0
+
+	16.如果参数定义了一个import operation，且external handle 是一个Android hardware buffer，且pNext中包含一个image 不为VK_NULL_HANDLE的VkMemoryDedicatedAllocateInfo，则（1）Android hardware buffer的AHardwareBuffer::usage 至少要包含AHARDWAREBUFFER_USAGE_GPU_FRAMEBUFFER, AHARDWAREBUFFER_USAGE_GPU_SAMPLED_IMAGE 或者 AHARDWAREBUFFER_USAGE_GPU_DATA_BUFFER 其中一个
+																																									    （2）对于Android hardware buffer， image的format必须为VK_FORMAT_UNDEFINED 或者通过vkGetAndroidHardwareBufferPropertiesANDROID返回的VkAndroidHardwareBufferFormatPropertiesANDROID::format
+																																									    （3）image的width, height, 以及 array layer 维数必须和 Android hardware buffer’s AHardwareBuffer_Desc中的对应参数相同
+																																									    （4）如果Android hardware buffer的 AHardwareBuffer::usage 包含AHARDWAREBUFFER_USAGE_GPU_MIPMAP_COMPLETE ，则image必须有一个完成的mipmap链（即mipmap一直到width和height都为1 ？）
+																																									    （5）如果Android hardware buffe的 AHardwareBuffer::usage不包含AHARDWAREBUFFER_USAGE_GPU_MIPMAP_COMPLETE，则iamge必须只有一个mipmap级别
+																																									    （6）每一个设置为image 的usage的比特位必须罗列在AHardwareBuffer Usage Equivalence 中，如果没有在该列表中，则该比特位必须包含在Android hardware buffer的 AHardwareBuffer_Desc::usage 中
+
+	17.如果参数定义了一个import operation，且external handle 类型为VK_EXTERNAL_MEMORY_HANDLE_TYPE_SCREEN_BUFFER_BIT_QNX，则（1） VkPhysicalDeviceExternalMemoryScreenBufferFeaturesQNX::screenBufferImport必须开启
+																														   （2）allocationSize 必须是为QNX Screen buffer调用 vkGetScreenBufferPropertiesQNX 返回的大小
+																														   （3）memoryTypeIndex 必须是为QNX Screen buffer调用 vkGetScreenBufferPropertiesQNX 返回的参数中所对应参数的的一个索引
+	18.如果参数定义了一个import operation，且external handle 为一个QNX Screen buffer，且pNext中包含一个image 不为VK_NULL_HANDLE的VkMemoryDedicatedAllocateInfo，则（1）QNX Screen’s buffer必须是一个有效的QNX Screen buffer
+																																								  （2）image的format必须为VK_FORMAT_UNDEFINED 或者为 QNX Screen buffer 通过vkGetScreenBufferPropertiesQNX返回的VkScreenBufferFormatPropertiesQNX::format
+																																								  （3）image的width, height, 以及 array layer 维数必须和 QNX Screen buffer的 _screen_buffer中的对应参数相同
+	19.如果VkMemoryOpaqueCaptureAddressAllocateInfo::opaqueCaptureAddress 不为 0，VkMemoryAllocateFlagsInfo::flags必须包含VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_CAPTURE_REPLAY_BIT
+	20.如果VkMemoryAllocateFlagsInfo::flags包含VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_CAPTURE_REPLAY_BIT，则bufferDeviceAddressCaptureReplay 特性必须开启
+	21.如果VkMemoryAllocateFlagsInfo::flags包含VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_BIT，则bufferDeviceAddress 特性必须开启
+	22.如果pNext中包含VkImportMemoryHostPointerInfoEXT，则VkMemoryOpaqueCaptureAddressAllocateInfo::opaqueCaptureAddress 必须为 0
+	23.如果参数定义了一个import operation，VkMemoryOpaqueCaptureAddressAllocateInfo::opaqueCaptureAddress 必须为 0
+	24.如果参数定义了一个import operation，且external handle 类型为VK_EXTERNAL_MEMORY_HANDLE_TYPE_ZIRCON_VMO_BIT_FUCHSIA，则（1）memoryTypeIndex 必须是vkGetMemoryZirconHandlePropertiesFUCHSIA 返回信息VkMemoryZirconHandlePropertiesFUCHSIA 中对应参数memoryTypeBits 中的一个的索引
+																															（2）allocationSize 必须大于 0 且小于等于通过zx_vmo_get_size(handle)获取的VMO大小，其中handle为引入外部内存的VMO handle
+	25.如果pNext中包含一个VkExportMetalObjectCreateInfoEXT， 则该结构体的exportObjectType必须为VK_EXPORT_METAL_OBJECT_TYPE_METAL_BUFFER_BIT_EXT
+
+
+
+	*/
+	
+	
+	
+	
+	
 	vkAllocateMemory(device, &memoryAllocateInfo, nullptr, &deviceMemory);
 	/*
 	vkAllocateMemory有效用法:
